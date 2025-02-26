63be2e3bc36ce70f48fae1ca205e555e033a4b98
Maria Eduarda F. Teixeira <138616948+Maria-Eduarda-FT@users.noreply.github.com>
2025-02-26 11:35:39 -0300

    gif + mata mosquito

--- TrabFinal/TelaInicial/InicioView.swift
+++ TrabFinal/TelaInicial/InicioView.swift
@@ -6,6 +6,24 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct GifImageView: UIViewRepresentable {
+    private let name: String
+    init(_ name: String) {
+        self.name = name
+    }
+    func makeUIView(context: Context) -> WKWebView {
+        let webview = WKWebView()
+        let url = Bundle.main.url(forResource: name, withExtension: "gif")!
+        let data = try! Data(contentsOf: url)
+        webview.load(data, mimeType: "image/gif", characterEncodingName: "UTF-8", baseURL: url.deletingLastPathComponent())
+        return webview
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.reload()
+    }
+}
 
 struct Noticia: Hashable{
     //let id = UUID()
@@ -14,7 +32,7 @@ struct Noticia: Hashable{
 }
 
 struct InicioView: View {
-    
+    @State private var mata = false
     var noticias : [Noticia] =  [
         Noticia(titulo: "Noticia 1", texto: "Belo Horizonte é bonita"),  Noticia(titulo: "Noticia 2", texto: "Monte Carmelo é bonita")
     ]
@@ -24,31 +42,49 @@ struct InicioView: View {
             ZStack{
                 Color(.azulCinza).ignoresSafeArea()
                 VStack{
-                    VStack{
-                        Text("ProtozoAIro")
-                            .font(.custom("PixelatedFont", size: 28))
-                            .padding()
-                        Spacer()
-                        Image("mosquito")
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                            .frame(width: 200, height: 200)
-                        Spacer()
-                    }                        
+                    VStack {
+                        ZStack {
+                            Color.azulCinza.edgesIgnoringSafeArea(.all)
+                            Circle()
+                                .foregroundColor(.white)
+                                .frame(width: 210, height: 210)
+                                .offset(CGSize(width: 0, height: 120.0))
+                            
+                            if (!mata){
+                                GifImageView("gifMosquito")
+                                    .scaledToFit()
+                                    .clipShape(Circle())
+                                    .frame(width: 190, height: 190)
+                                    .offset(CGSize(width: 0, height: 120.0))
+                                    .onTapGesture {
+                                        mata = true
+                                    }
+                            }else {
+                                Image("morto")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .clipShape(Circle())
+                                    .frame(width: 250, height: 250)
+                                    .offset(CGSize(width: 0, height: 120.0))
+                                
+                            }
+                        }//zstack inner
+                        .frame(height: 200)
+                    }//vstack cima
+                    .zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
                     VStack {
                         VStack{
                             if let randomNoticia = noticias.randomElement(){
-                                Text("\(randomNoticia.titulo)").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).padding().foregroundColor(.white)
+                                Text("\(randomNoticia.titulo)").font(.title).padding().foregroundColor(.white)
                                 Text("\(randomNoticia.texto)").foregroundColor(.white)
                             }//if
                         }//vstackdentro
                         .padding()
                         .background(Color(.azulCinza))
                         .cornerRadius(15)
-                        }//vstack
-                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                        .background().edgesIgnoringSafeArea(.bottom)
+                    }//vstack baixo
+                    .frame(maxWidth: .infinity/*@END_MENU_TOKEN@*/,maxHeight: /*@START_MENU_TOKEN@*/.infinity)
+                    .background().edgesIgnoringSafeArea(.bottom)
                     
                 }//vstackmain
             }//zstack
