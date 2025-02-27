2e41afe15ee0f3184f71d75f15bb76949f3f020a
Maria Eduarda F. Teixeira <138616948+Maria-Eduarda-FT@users.noreply.github.com>
2025-02-27 10:41:39 -0300

    Update InfoView.swift

--- TrabFinal/TelaInicial/InfoView.swift
+++ TrabFinal/TelaInicial/InfoView.swift
@@ -1,15 +1,42 @@
-//
-//  InfoView.swift
-//  TelaInicial
-//
-//  Created by Turma01-21 on 25/02/25.
-//
-
 import SwiftUI
+import WebKit
+
+struct YouTubeView: UIViewRepresentable {
+    let videoId: String
+    func makeUIView(context: Context) ->  WKWebView {
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let demoURL = URL(string: "https://www.youtube.com/embed/\(videoId)") else { return }
+        uiView.scrollView.isScrollEnabled = false
+        uiView.load(URLRequest(url: demoURL))
+    }
+}
 
 struct InfoView: View {
+    @State var id = "9LhfL3Dg_lA"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            VStack{
+                VStack {
+                    ZStack {
+                        Color.azulCinza.edgesIgnoringSafeArea(.all)
+                        Circle()
+                            .foregroundColor(.white)
+                            .frame(width: 210, height: 210)
+                            .offset(CGSize(width: 0, height: 120.0))
+                    }
+                    .frame(height: 200)
+                }
+                Spacer()
+                YouTubeView(videoId: id)
+                    .cornerRadius(15)
+                    .frame(width: 350, height: 250)
+                    .padding()
+                Spacer()
+                
+            }
+            }
     }
 }
 
